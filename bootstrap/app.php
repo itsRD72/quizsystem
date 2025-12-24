c34b13729a7e919c6971b19e9424c7a97dd7a390
Raj Chavda <rickroy121272@gmail.com>
2025-12-24 09:49:22 +0530

    all main things are done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckUserAuth;
+use App\Http\Middleware\CheckAdminAuth;
 
 
 return Application::configure(basePath: dirname(__DIR__))
